22e06165cb4dfc075dcb0277422cb2627357e2e7
Adam Bieszk <adambieszk6@gmail.com>
2026-01-31 03:45:33 +0100

    feat(hair): add hair for simon

--- main.lua
+++ main.lua
@@ -1,1 +1,16 @@
 local SimonCharacterMod = RegisterMod("Simon Character Mod",1)
+
+local simonPlayerType = Isaac.GetPlayerTypeByName("Simon", false)
+local hairCostume = Isaac.GetCostumeIdByPath("gfx/characters/simon_hair.anm2")
+
+---Add costumes on init for Simon
+---@param player EntityPlayer
+function SimonCharacterMod:GiveCostumesOnInit(player)
+    if player:GetPlayerType() ~= simonPlayerType then
+        return
+    end
+
+    player:AddNullCostume(hairCostume)
+end
+
+SimonCharacterMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, SimonCharacterMod.GiveCostumesOnInit)
